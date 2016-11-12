7a5e5ab320e78903c32c3df8350446793d081bb3
Ed Wilson <dlogik@gmail.com>
2016-11-13 10:06:14 +1100

    Added oidc-client and babel-polyfill packages. Added test UserManager to app.component.ts file.

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Config } from './shared/index';
 import './operators';
 
+import { UserManager } from 'oidc-client/oidc-client';
+
 /**
  * This class represents the main application component. Within the @Routes annotation is the configuration of the
  * applications routes, configuring the paths for the lazy loaded components (HomeComponent, AboutComponent).
@@ -15,5 +17,25 @@ import './operators';
 export class AppComponent {
   constructor() {
     console.log('Environment config', Config);
+
+    let mgr: UserManager = new UserManager(settings);
+
+    console.log(mgr);
   }
 }
+
+const settings: any = {
+  authority: 'http://localhost:5000/oidc',
+  client_id: 'js.tokenmanager',
+  redirect_uri: 'http://localhost:4200/auth.html',
+  post_logout_redirect_uri: 'http://localhost:4200/',
+  response_type: 'id_token token',
+  scope: 'openid email roles',
+
+  silent_redirect_uri: 'http://localhost:4200',
+  automaticSilentRenew: true,
+  //silentRequestTimeout:10000,
+
+  filterProtocolClaims: true,
+  loadUserInfo: true
+};
