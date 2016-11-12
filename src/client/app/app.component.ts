1e3e986f2bb3a8f47604b895f6f8567e9ccc20fd
Ed Wilson <dlogik@gmail.com>
2016-11-13 10:18:46 +1100

    Fixed semantic errors

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -4,6 +4,22 @@ import './operators';
 
 import { UserManager } from 'oidc-client/oidc-client';
 
+const settings: any = {
+  authority: 'http://localhost:5000/oidc',
+  client_id: 'js.tokenmanager',
+  redirect_uri: 'http://localhost:4200/auth.html',
+  post_logout_redirect_uri: 'http://localhost:4200/',
+  response_type: 'id_token token',
+  scope: 'openid email roles',
+
+  silent_redirect_uri: 'http://localhost:4200',
+  automaticSilentRenew: true,
+  //silentRequestTimeout:10000,
+
+  filterProtocolClaims: true,
+  loadUserInfo: true
+};
+
 /**
  * This class represents the main application component. Within the @Routes annotation is the configuration of the
  * applications routes, configuring the paths for the lazy loaded components (HomeComponent, AboutComponent).
@@ -23,19 +39,3 @@ export class AppComponent {
     console.log(mgr);
   }
 }
-
-const settings: any = {
-  authority: 'http://localhost:5000/oidc',
-  client_id: 'js.tokenmanager',
-  redirect_uri: 'http://localhost:4200/auth.html',
-  post_logout_redirect_uri: 'http://localhost:4200/',
-  response_type: 'id_token token',
-  scope: 'openid email roles',
-
-  silent_redirect_uri: 'http://localhost:4200',
-  automaticSilentRenew: true,
-  //silentRequestTimeout:10000,
-
-  filterProtocolClaims: true,
-  loadUserInfo: true
-};
